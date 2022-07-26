d43bd7a1610084a400a84ee44371b0b699f749e3
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2022-07-26 15:24:19 +0300

    Added a few utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   mode: 'jit',
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+
+      // adding smaller screens
+      screens: {
+        'xs': '275px'
+      },
+
+
+      // adding a new utility class to make 7 rows
+      gridTemplateRows: {
+
+        '7': 'repeat(7, minmax(0, 1fr))',
+
+      },
+
+    },
   },
   plugins: [],
 }
